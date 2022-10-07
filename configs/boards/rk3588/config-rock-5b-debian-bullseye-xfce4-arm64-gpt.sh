f6024fac6086f3b80649828adea74e61b178ecc9
kylin <kylin_fedora@hotmail.com>
2022-10-07 16:40:18 +0800

    Update config-rock-5b-debian-bullseye-xfce4-arm64-gpt.sh

--- configs/boards/rk3588/config-rock-5b-debian-bullseye-xfce4-arm64-gpt.sh
+++ configs/boards/rk3588/config-rock-5b-debian-bullseye-xfce4-arm64-gpt.sh
@@ -45,7 +45,7 @@ cat > $BUILD_DIR/${BOARD}-${MODEL}-${DISTRO}-${VARIANT}-${ARCH}-${FORMAT}-yaml.l
 20_packages_start.yaml
 21_packages_base_mini.yaml
 21_packages_devel.yaml
-21_packages_libs.yaml
+21_packages_libs_mini.yaml
 21_packages_kernel.yaml
 21_packages_math.yaml
 21_packages_mpp.yaml
